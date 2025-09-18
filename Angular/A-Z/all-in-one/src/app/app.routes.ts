ef04610b6d798a4af03a5922cf6fc57e81802104
Soheyl Rahgozar <soheyl.rahgozar@gmail.com>
2025-09-18 00:55:05 -0400

    routerLink  activatedRout pass id as query param

--- Angular/A-Z/all-in-one/src/app/app.routes.ts
+++ Angular/A-Z/all-in-one/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router'
 import { HomeComponent } from './home-component/home-component'
 import { Parent } from './parent/parent'
 import { School } from './school/school'
+import { User } from './user/user'
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'parent', component: Parent },
     { path: 'school', component: School },
+    { path: 'user/:id', component: User },
 ]
--- Angular/A-Z/all-in-one/src/app/home-component/home-component.ts
+++ Angular/A-Z/all-in-one/src/app/home-component/home-component.ts
@@ -1,20 +1,25 @@
 import { Component, OnInit } from '@angular/core'
 import { ApiService } from '../api-service'
 import { JsonPipe } from '@angular/common'
+import { Router, RouterLink } from '@angular/router'
 
 @Component({
     selector: 'app-home-component',
-    imports: [JsonPipe],
+    imports: [JsonPipe, RouterLink],
     templateUrl: './home-component.html',
     styleUrl: './home-component.css',
 })
 export class HomeComponent implements OnInit {
     data: any
-    constructor(private api: ApiService) {}
+    constructor(private api: ApiService, private router: Router) {}
 
     ngOnInit(): void {
         this.api
             .getData('https://jsonplaceholder.typicode.com/todos/1')
             .subscribe((res) => (this.data = res))
+    }
+
+    goSchool() {
+        this.router.navigate(['/school'])
     }
 }
