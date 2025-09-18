0f69a4da31f5f4c8e9f396485c1196f5d3877174
Soheyl Rahgozar <soheyl.rahgozar@gmail.com>
2025-09-18 00:30:55 -0400

    viewChildern and queryList child component

--- Angular/A-Z/all-in-one/src/app/school/school.ts
+++ Angular/A-Z/all-in-one/src/app/school/school.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core'
+import { Component, QueryList, ViewChildren } from '@angular/core'
 import { Student } from '../student/student'
+import { CommonModule } from '@angular/common'
 
 @Component({
     selector: 'app-school',
-    imports: [Student],
+    imports: [Student, CommonModule],
     templateUrl: './school.html',
     styleUrl: './school.css',
 })
-export class School {}
+export class School {
+    names: string[] = []
+    @ViewChildren(Student) student!: QueryList<Student>
+
+    listStudents() {
+        this.names = this.student.map((child) => child.name)
+    }
+}
